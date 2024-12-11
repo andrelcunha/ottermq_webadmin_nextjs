6fd2235cc2eead1899d2c2578999c685fb94004f
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-12-11 00:04:49 -0300

    feat: send message through exchange page

--- src/app/exchanges/page.tsx
+++ src/app/exchanges/page.tsx
@@ -25,26 +25,15 @@ async function getBindings(exchange: string): Promise<Bindings> {
     return data.bindings;
 }
 
-const handleUnbind = async (exchange: string, routingKey: string, queue: string) => {
-    const response = await fetch('http://localhost:8081/bindings', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ exchange_name: exchange, routing_key: routingKey, queue_name: queue }),
-    });
-
-    if (!response.ok) {
-        console.error('Failed to unbind the queue');
-    }
-};
-
 const Page = () => {
     const [exchanges, setExchanges] = useState<string[]>([]);
     const [bindings, setBindings] = useState<Bindings>({});
-    const [selectedExchange, setSelectedExchange] = useState<string | null>(null);
+    const [selectedExchange, setSelectedExchange] = useState<string>("default");
     const [newQueue, setNewQueue] = useState<string>('');
     const [newRoutingKey, setNewRoutingKey] = useState<string>('');
+    const [msgRoutingKey, setMsgRoutingKey] = useState<string>('');
+    const [payload, setPayload] = useState<string>('');
+
 
     useEffect(() => {
         const fetchExchanges = async () => {
@@ -66,19 +55,77 @@ const Page = () => {
         }
     }, [selectedExchange]);
 
-    const handleAddBinding = () => {
-        console.log(`Bind ${newQueue} to ${newRoutingKey}`);
+    const handleAddBinding = async() => {
+        if (!selectedExchange || !newQueue || !newRoutingKey) {
+            console.error('Please fill in all fields');
+            return;
+        }
+
+        const response = await fetch('http://localhost:8081/bindings', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({exchange_name: selectedExchange, routing_key: newRoutingKey, queue_name: newQueue}),
+                
+        });
+
+        if (response.ok) {
+            console.log('Bind ${newQueue} to ${selectedExchange} with routing key ${newRoutingKey}');
+            const updatedBindings = await getBindings(selectedExchange);
+            setBindings(updatedBindings);
+        } else {
+            console.error('Failed to add the binding');
+        }
+
+    };
+
+    const handleUnbind = async (exchange: string, routingKey: string, queue: string) => {
+        const response = await fetch('http://localhost:8081/bindings', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ exchange_name: exchange, routing_key: routingKey, queue_name: queue }),
+        });
+    
+        if (response.ok) {
+            console.log('Queue ${queue} unbound to ${exchange}');
+            const updatedBindings = await getBindings(exchange);
+            setBindings(updatedBindings);
+        } else {
+            console.error('Failed to unbind the queue');
+        }
+    
+    };
+
+    const handlePublishMessage = async () => {
+        const exchange = selectedExchange || 'default';
+
+        const response = await fetch('http://localhost:8081/messages', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ exchange_name: exchange, routing_key: msgRoutingKey, payload: payload }),
+        });
+
+        if (response.ok) {
+            console.log('Message sebt to ${exchange} with routing key ${msgRoutingKey}');
+        } else {
+            console.error('Failed to publish the message');
+        }
     };
 
     return (
         <div>
             <h1 className="text-2xl font-bold mb-4">Exchanges</h1>
-            <table className="min-w-full border border-gray-800">
+            <table className="min-w-full border border-gray-900">
                 <thead>
                     <tr>
-                        <th className="py-3 border border-gray-800">Virtual host</th>
-                        <th className="py-3 border border-gray-800">Name</th>
-                        <th className="py-3 border border-gray-800">Type</th>
+                        <th className="py-3 border border-gray-900">Virtual host</th>
+                        <th className="py-3 border border-gray-900">Name</th>
+                        <th className="py-3 border border-gray-900">Type</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -89,9 +136,9 @@ const Page = () => {
                             onClick={() => setSelectedExchange(exchange)}
                             style={{ cursor: 'pointer' }}
                         >
-                            <td className="py-2 px-4 border border-gray-800">localhost</td>
-                            <td className="py-2 px-4 border border-gray-800">{exchange}</td>
-                            <td className="py-2 px-4 border border-gray-800">direct</td>
+                            <td className="py-2 px-4 border border-gray-900">localhost</td>
+                            <td className="py-2 px-4 border border-gray-900">{exchange}</td>
+                            <td className="py-2 px-4 border border-gray-900">direct</td>
                         </tr>
                     ))}
                 </tbody>
@@ -100,21 +147,21 @@ const Page = () => {
             {selectedExchange && (
                 <>
                     <h2 className="text-xl font-bold mt-6 mb-4">Bindings for {selectedExchange}</h2>
-                    <table className="min-w-full border border-gray-800 mb-4">
+                    <table className="min-w-full border border-gray-900 mb-4">
                         <thead>
                             <tr>
-                                <th className="py-2 border">Routing Key</th>
-                                <th className="py-2 border">Queue</th>
-                                <th className="py-2 border"></th>
+                                <th className="py-2 border border-gray-900">Routing Key</th>
+                                <th className="py-2 border border-gray-900">Queue</th>
+                                <th className="py-2 border border-gray-900"></th>
                             </tr>
                         </thead>
                         <tbody>
                             {Object.entries(bindings).map(([routingKey, queues], index) => (
                                 queues.map((queue, queueIndex) => (
                                     <tr key={`${index}-${queueIndex}`} className={`border ${index % 2 === 0 ? 'bg-gray-800' : ''}`}>
-                                        <td className="py-2 px-4 border">{routingKey}</td>
-                                        <td className="py-2 px-4 border">{queue}</td>
-                                        <td className="py-2 px-4 border">
+                                        <td className="py-2 px-4 border border-gray-900">{routingKey}</td>
+                                        <td className="py-2 px-4 border border-gray-900">{queue}</td>
+                                        <td className="py-2 px-4 border border-gray-900">
                                             <button className="bg-gray-700 text-white px-2 py-1 rounded" onClick={() => handleUnbind(selectedExchange, routingKey, queue)}>Unbind</button>
                                         </td>
                                     </tr>
@@ -132,7 +179,7 @@ const Page = () => {
                     type="text"
                     value={newQueue}
                     onChange={(e) => setNewQueue(e.target.value)}
-                    className="border p-2 w-full"
+                    className="border p-2 w-full text-black"
                 />
             </div>
             <div className="mb-4">
@@ -141,10 +188,32 @@ const Page = () => {
                     type="text"
                     value={newRoutingKey}
                     onChange={(e) => setNewRoutingKey(e.target.value)}
-                    className="border p-2 w-full"
+                    className="border p-2 w-full text-black"
                 />
             </div>
             <button className="bg-gray-700 text-white px-4 py-2 rounded" onClick={handleAddBinding}>Bind</button>
+
+            <h1 className="text-2xl font-bold mt-8 mb-4">Publish Message</h1>
+            <p className="mb-4">
+                The message will be published to the exchange <b>{selectedExchange}</b> with the given routing key.
+            </p>
+
+            <div className="mb-4">
+            <input
+                    type="text"
+                    value={msgRoutingKey}
+                    onChange={(e) => setMsgRoutingKey(e.target.value)}
+                    className="border p-2 w-full text-black"
+                />
+                <label className="block mb-2">Payload:</label>
+                <input
+                    type="text"
+                    value={payload}
+                    onChange={(e) => setPayload(e.target.value)}
+                    className="border p-2 w-full text-black"
+                />
+            </div>
+            <button className="bg-gray-700 text-white px-4 py-2 rounded" onClick={handlePublishMessage}>Publish</button>
         </div>
     );
 };
